d87bc7b75f13f3e54847dae2ec7aeeff74666bb3
Tiryoh <tiryoh@gmail.com>
2019-08-29 14:12:58 +0900

    Fix TravisCI settings

--- .travis/prepare_sync.sh
+++ .travis/prepare_sync.sh
@@ -4,6 +4,6 @@ set -eu
 
 mkdir -p ~/.ssh && touch ~/.ssh/config
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n\tIdentityFile ~/.ssh/id_rsa\n\tPort 22\n" >> ~/.ssh/config
-openssl aes-256-cbc -K $encrypted_02c8af4b131a_key -iv $encrypted_02c8af4b131a_iv -in ./.travis/github_deploy_key.enc -out ./.travis/id_rsa -d
+openssl aes-256-cbc -K ${encrypted_02c8af4b131a_key} -iv ${encrypted_02c8af4b131a_iv} -in ./.travis/github_deploy_key.enc -out ./.travis/id_rsa -d
 chmod 600 ./.travis/id_rsa
 cp ./.travis/id_rsa ~/.ssh/
--- .travis/sync.sh
+++ .travis/sync.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-set -eu
+set -eux
 
 git config --local user.name "RT Corp."
 git config --local user.email "shop@rt-net.jp"
-git remote remove origin
-git remote add origin git@github.com:rt-net/RaspberryPiMouse-Wiki.git > /dev/null 2>&1
 git remote add upstream git@github.com:rt-net/RaspberryPiMouse.wiki.git > /dev/null 2>&1
 git fetch origin
 git fetch upstream
 git merge upstream/master --no-edit
-git push origin HEAD:master > /dev/null 2>&1
 git push upstream HEAD:master > /dev/null 2>&1
