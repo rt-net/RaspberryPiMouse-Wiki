2c00da93ea98385f4858b38fb8d857644ffff883
Tiryoh <tiryoh@gmail.com>
2019-08-29 13:54:46 +0900

    Fix sync script

--- .travis/sync.sh
+++ .travis/sync.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
-git config user.name "RT Corp."
-git config user.email "shop@rt-net.jp"
+git config --local user.name "RT Corp."
+git config --local user.email "shop@rt-net.jp"
 git remote remove origin
-git remote add origin https://$GITHUB_API_KEY@github.com/rt-net/RaspberryPiMouse-Wiki.git > /dev/null 2>&1
-git remote add upstream https://$GITHUB_API_KEY@github.com/rt-net/RaspberryPiMouse.wiki.git > /dev/null 2>&1
+git remote add origin git@github.com:rt-net/RaspberryPiMouse-Wiki.git > /dev/null 2>&1
+git remote add upstream git@github.com:rt-net/RaspberryPiMouse.wiki.git > /dev/null 2>&1
 git fetch origin
 git fetch upstream
 git merge upstream/master --no-edit
